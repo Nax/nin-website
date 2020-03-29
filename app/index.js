d84f6b6929bd81e0ba9342c839296d4ff9c5f8c9
Nax <max@bacoux.com>
2020-03-29 15:57:25 +0200

    Initial version

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import './index.css';
+
+const { userAgent } = navigator;
+let os = null;
+
+if (userAgent.match("Windows NT") || userAgent.match("Win32") || userAgent.match("Win64")) {
+  if (userAgent.match("Win64") || userAgent.match("x64;")) {
+    os = 'win64';
+  } else {
+    os = 'win32';
+  }
+} else if (userAgent.match("MacIntel")) {
+  os = 'mac';
+}
+
+if (os) {
+  const id = 'download-' + os;
+  const el = document.getElementById(id);
+  if (el) {
+    el.classList.add('prefered');
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const env = process.env.NODE_ENV || 'development';
+const dev = (env !== 'production');
+
+const VERSION = '0.7.0';
+
+module.exports = {
+  mode: (dev ? 'development' : 'production'),
+  entry: {
+    app: './app/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: (dev ? '[name].js' : '[name].[contenthash].min.js'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      loaders: [
+        dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        { loader: 'css-loader', options: { importLoaders: 1 } },
+        'postcss-loader'
+      ]
+    }, {
+      test: /\.(png|gif|jpe?g|svg)$/,
+      use: { loader: 'file-loader', options: { name: (dev ? '_assets/[name].[ext]' : '_assets/[contenthash].[ext]') } }
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './app/assets/logo.png',
+      cache: true,
+      prefix: '_assets/',
+      favicons: {
+        appName: 'Nin',
+        appDescription: 'A NES Emulator',
+        developerName: 'Maxime Bacoux',
+        developerURL: 'https://nax.io',
+        icons: {
+          android: false,
+          appleIcon: false,
+          appleStartup: false,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          windows: false,
+          yandex: false
+        }
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].min.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './app/index.html.ejs',
+      templateParameters: { VERSION }
+    })
+  ]
+};
